29d53453fec2f08559340fd8e88411b0f19d51c1
Jeremy Adam <underway113@gmail.com>
2019-06-13 17:38:10 +0700

    Add Dot

--- Measure1/ViewController.swift
+++ Measure1/ViewController.swift
@@ -22,12 +22,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,29 +44,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLoc = touches.first?.location(in: sceneView) {
+            let hitTestResuls = sceneView.hitTest(touchLoc, types: .featurePoint)
+            
+            if let hitResult = hitTestResuls.first {
+                addDot(hitResult)
+            }
+            
+        }
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func addDot(_ hitResult:ARHitTestResult) {
+        let dotGeo = SCNSphere(radius: 0.003)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeo.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeo)
+        
+        dotNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y, z: hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
